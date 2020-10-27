16a2a89fa4ea0e8cbea6d94804b58b71df83baed
GhostTheBoo <ghostthebooyt@gmail.com>
2020-10-27 13:46:30 -0700

    Added equipment replace and default functionality

--- javascript/main.js
+++ javascript/main.js
@@ -2,33 +2,42 @@
 function initialize() {
 	console.log(currentPage);
 	switch (currentPage) {
-		case 'bonus':
+		case 'bonus': {
 			break;
-		case 'chests':
+		}
+		case 'chests': {
 			addWorlds();
 			addRewardTypes();
 			populateTable(0);
 			break;
-		case 'equipment':
+		}
+		case 'equipment': {
 			addEquipmentTypes();
 			addRewardTypes();
 			populateTable(0);
 			break;
-		case 'forms':
+		}
+		case 'forms': {
 			break;
-		case 'levels':
+		}
+		case 'levels': {
 			break;
-		case 'other':
+		}
+		case 'other': {
 			break;
-		case 'popups':
+		}
+		case 'popups': {
 			addWorlds();
 			addRewardTypes();
 			populateTable(0);
 			break;
-		case 'index':
+		}
+		case 'index': {
 			break;
-		default:
+		}
+		default: {
 			break;
+		}
 	}
 }
 
@@ -54,13 +63,17 @@ function addEquipmentTypes() {
 	}
 }
 
-// populate table with selected world's reward locations
+// populate table with selected collection's locations
 function populateTable(ID) {
-	let table = document.getElementById(currentPage + 'Table');
-	let oldTableBody = table.lastElementChild;
-	let newTableBody = document.createElement('tbody');
+	let table;
+	let newTableBody;
 	switch (currentPage) {
-		case 'chests':
+		case 'bonus': {
+			break;
+		}
+		case 'chests': {
+			table = document.getElementById('chestsTable');
+			newTableBody = document.createElement('tbody');
 			if (chestArray[ID].Chests.length > 0) {
 				for (let i = 0; i < chestArray[ID].Chests.length; i++) {
 					let row = document.createElement('tr');
@@ -78,7 +91,41 @@ function populateTable(ID) {
 				}
 			}
 			break;
-		case 'popups':
+		}
+		case 'equipment': {
+			table = document.getElementById('equipmentTable');
+			newTableBody = document.createElement('tbody');
+			table = document.getElementById(currentPage + 'Table');
+			if (equipmentArray[ID].Equipments.length > 0) {
+				for (let i = 0; i < equipmentArray[ID].Equipments.length; i++) {
+					let row = document.createElement('tr');
+					row.id = 'row' + i;
+					let cell = document.createElement('input');
+					cell.type = 'checkbox';
+					cell.id = 'check' + i;
+					row.appendChild(cell);
+					equipmentPropertiesArray.forEach(property => {
+						let cell = document.createElement('td');
+						cell.innerHTML = equipmentArray[ID].Equipments[i][property];
+						row.appendChild(cell);
+					})
+					newTableBody.appendChild(row);
+				}
+			}
+			break;
+		}
+		case 'forms': {
+			break;
+		}
+		case 'levels': {
+			break;
+		}
+		case 'other': {
+			break;
+		}
+		case 'popups': {
+			table = document.getElementById('popupsTable');
+			newTableBody = document.createElement('tbody');
 			if (popupArray[ID].Popups.length > 0) {
 				for (let i = 0; i < popupArray[ID].Popups.length; i++) {
 					let row = document.createElement('tr');
@@ -96,24 +143,12 @@ function populateTable(ID) {
 				}
 			}
 			break;
-		case 'equipment':
-			table = document.getElementById(currentPage + 'Table');
-			if (equipmentArray[ID].Equipments.length > 0) {
-				for (let i = 0; i < equipmentArray[ID].Equipments.length; i++) {
-					let row = document.createElement('tr');
-					row.id = 'row' + i;
-					equipmentPropertiesArray.forEach(property=>{
-						let cell = document.createElement('td');
-						cell.innerHTML = equipmentArray[ID].Equipments[i][property];
-						row.appendChild(cell);
-					})
-					newTableBody.appendChild(row);
-				}
-			}
+		}
+		default: {
 			break;
-		default:
-			break;
+		}
 	}
+	let oldTableBody = table.lastElementChild;
 	table.replaceChild(newTableBody, oldTableBody);
 }
 
@@ -151,77 +186,167 @@ function removeOptions(selectElement) {
 
 // add reward and reward address to location
 function replace() {
-	let rt = document.getElementById('rewardTypeSelector');
-	let r = document.getElementById('rewardSelector');
-	let w = document.getElementById('worldSelector');
-	let rowCount = document.getElementById(currentPage + 'Table').rows.length;
-	let locationArray;
-
 	switch (currentPage) {
-		case 'bonus':
+		case 'bonus': {
 			break;
-		case 'chests':
-			locationArray = chestArray[w.value].Chests;
+		}
+		case 'chests': {
+			let rt = document.getElementById('rewardTypeSelector');
+			let r = document.getElementById('rewardSelector');
+			let w = document.getElementById('worldSelector');
+			let rowCount = document.getElementById('chestsTable').rows.length;
+			let locationArray = chestArray[w.value].Chests;
+			for (let i = 0; i < rowCount - 1; i++) {
+				let checked = document.getElementById('check' + i).checked;
+				if (checked) {
+					locationArray[i]['Replacement Reward'] = rewardArray[rt.value].Rewards[r.value]['Reward'];
+					locationArray[i]['Replacement Address'] = rewardArray[rt.value].Rewards[r.value]['Reward Address'];
+				}
+			}
+			populateTable(w.value);
 			break;
-		case 'equipment':
+		}
+		case 'equipment': {
+			let rt = document.getElementById('rewardTypeSelector');
+			let r = document.getElementById('rewardSelector');
+			let et = document.getElementById('equipmentTypeSelector');
+			let locationArray = equipmentArray[et.value].Equipments;
+			let str = document.getElementById('strength');
+			let mag = document.getElementById('magic');
+			let ap = document.getElementById('ap');
+			let def = document.getElementById('defense');
+			let physical = document.getElementById('physicalResistance');
+			let fire = document.getElementById('fireResistance');
+			let blizzard = document.getElementById('blizzardResistance');
+			let thunder = document.getElementById('thunderResistance');
+			let dark = document.getElementById('darkResistance');
+			let light = document.getElementById('lightResistance');
+			let universal = document.getElementById('universalResistance');
+			let rowCount = document.getElementById('equipmentTable').rows.length;
+			for (let i = 0; i < rowCount - 1; i++) {
+				let checked = document.getElementById('check' + i).checked;
+				if (checked) {
+					locationArray[i]['Ability'] = rewardArray[rt.value].Rewards[r.value]['Reward'];
+					locationArray[i]['Replacement Ability Address'] = rewardArray[rt.value].Rewards[r.value]['Reward Address'];
+					locationArray[i]['Strength'] = str.value;
+					locationArray[i]['Magic'] = mag.value;
+					locationArray[i]['AP'] = ap.value;
+					locationArray[i]['Defense'] = def.value;
+					locationArray[i]['Physical Resistance'] = physical.value;
+					locationArray[i]['Fire Resistance'] = fire.value;
+					locationArray[i]['Blizzard Resistance'] = blizzard.value;
+					locationArray[i]['Thunder Resistance'] = thunder.value;
+					locationArray[i]['Dark Resistance'] = dark.value;
+					locationArray[i]['Light Resistance'] = light.value;
+					locationArray[i]['Universal Resistance'] = universal.value;
+				}
+			}
+			populateTable(et.value);
 			break;
-		case 'forms':
+		}
+		case 'forms': {
 			break;
-		case 'levels':
+		}
+		case 'levels': {
 			break;
-		case 'other':
+		}
+		case 'other': {
 			break;
-		case 'popups':
-			locationArray = popupArray[w.value].Popups;
+		}
+		case 'popups': {
+			let rt = document.getElementById('rewardTypeSelector');
+			let r = document.getElementById('rewardSelector');
+			let w = document.getElementById('worldSelector');
+			let rowCount = document.getElementById('popupsTable').rows.length;
+			let locationArray = popupArray[w.value].Popups;
+			for (let i = 0; i < rowCount - 1; i++) {
+				let checked = document.getElementById('check' + i).checked;
+				if (checked) {
+					locationArray[i]['Replacement Reward'] = rewardArray[rt.value].Rewards[r.value]['Reward'];
+					locationArray[i]['Replacement Address'] = rewardArray[rt.value].Rewards[r.value]['Reward Address'];
+				}
+			}
+			populateTable(w.value);
 			break;
-		default:
+		}
+		default: {
 			break;
-	}
-
-	for (let i = 0; i < rowCount - 1; i++) {
-		let checked = document.getElementById('check' + i).checked;
-		if (checked) {
-			locationArray[i]['Replacement Reward'] = rewardArray[rt.value].Rewards[r.value]['Reward'];
-			locationArray[i]['Replacement Address'] = rewardArray[rt.value].Rewards[r.value]['Reward Address'];
 		}
 	}
-	populateTable(w.value);
 }
 
 // set replaced rewards to null
 function goldExperienceRequiem() {
-	let w = document.getElementById('worldSelector');
-	let rowCount = document.getElementById(currentPage + 'Table').rows.length;
-	let locationArray;
-
 	switch (currentPage) {
-		case 'bonus':
+		case 'bonus': {
 			break;
-		case 'chests':
-			locationArray = chestArray[w.value].Chests;
+		}
+		case 'chests': {
+			let w = document.getElementById('worldSelector');
+			let rowCount = document.getElementById('chestsTable').rows.length;
+			let locationArray = chestArray[w.value].Chests;
+			for (let i = 0; i < rowCount - 1; i++) {
+				let checked = document.getElementById('check' + i).checked;
+				if (checked) {
+					locationArray[i]['Replacement Reward'] = '';
+					locationArray[i]['Replacement Address'] = '';
+				}
+			}
+			populateTable(w.value);
 			break;
-		case 'equipment':
+		}
+		case 'equipment': {
+			let et = document.getElementById('equipmentTypeSelector');
+			let locationArray = equipmentArray[et.value].Equipments;
+			let rowCount = document.getElementById('equipmentTable').rows.length;
+			for (let i = 0; i < rowCount - 1; i++) {
+				let checked = document.getElementById('check' + i).checked;
+				if (checked) {
+					locationArray[i]['Ability'] = locationArray[i]['Vanilla Ability'];
+					locationArray[i]['Replacement Ability Address'] = '';
+					locationArray[i]['Strength'] = locationArray[i]['Vanilla Strength'];
+					locationArray[i]['Magic'] = locationArray[i]['Vanilla Magic'];
+					locationArray[i]['AP'] = locationArray[i]['Vanilla AP'];
+					locationArray[i]['Defense'] = locationArray[i]['Vanilla Defense'];
+					locationArray[i]['Physical Resistance'] = 0;
+					locationArray[i]['Fire Resistance'] = locationArray[i]['Vanilla Fire Resistance'];
+					locationArray[i]['Blizzard Resistance'] = locationArray[i]['Vanilla Blizzard Resistance'];
+					locationArray[i]['Thunder Resistance'] = locationArray[i]['Vanilla Thunder Resistance'];
+					locationArray[i]['Dark Resistance'] = locationArray[i]['Vanilla Dark Resistance'];
+					locationArray[i]['Light Resistance'] = 0;
+					locationArray[i]['Universal Resistance'] = 0;
+				}
+			}
+			populateTable(et.value);
 			break;
-		case 'forms':
+		}
+		case 'forms': {
 			break;
-		case 'levels':
+		}
+		case 'levels': {
 			break;
-		case 'other':
+		}
+		case 'other': {
 			break;
-		case 'popups':
-			locationArray = popupArray[w.value].Popups;
+		}
+		case 'popups': {
+			let w = document.getElementById('worldSelector');
+			let rowCount = document.getElementById('popupsTable').rows.length;
+			let locationArray = popupArray[w.value].Popups;
+			for (let i = 0; i < rowCount - 1; i++) {
+				let checked = document.getElementById('check' + i).checked;
+				if (checked) {
+					locationArray[i]['Replacement Reward'] = '';
+					locationArray[i]['Replacement Address'] = '';
+				}
+			}
+			populateTable(w.value);
 			break;
-		default:
+		}
+		default: {
 			break;
-	}
-	for (let i = 0; i < rowCount - 1; i++) {
-		let checked = document.getElementById('check' + i).checked;
-		if (checked) {
-			locationArray[i]['Replacement Reward'] = '';
-			locationArray[i]['Replacement Address'] = '';
 		}
 	}
-	populateTable(w.value);
 }
 
 // save replacement patch codes to pnach file
@@ -241,7 +366,21 @@ function save() {
 			}
 		}
 	}
-	//finalPnachStrings.push('\n')
+	finalPnachStrings.push('\n')
+
+	// Printing Equipment Replacements
+	finalPnachStrings.push('//Equipment Replacements\n')
+	for (let i = 0; i < equipmentTypeArray.length; i++) {
+		finalPnachStrings.push('// ' + equipmentTypeArray[i] + '\n')
+		for (let j = 0; j < equipmentArray[i].Equipments.length; j++) {
+			let equipment = equipmentArray[i].Equipments[j];
+			// ability replacement
+			// stat replacement
+			// elemental resistance replacement
+			// other resistance replacement
+		}
+	}
+	finalPnachStrings.push('\n')
 
 	// Printing Popup Replacements
 	finalPnachStrings.push('//Popup Replacements\n')
